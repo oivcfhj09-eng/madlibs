341ed3e27c275db52f017aeff6166cc1c1b29d5a
Jono <oivcfhj09@gmail.com>
2025-09-16 12:08:24 -0400

    initial commit

--- app.R
+++ app.R
@@ -31,7 +31,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   story <- eventReactive(input$submit, {
-    generate_story(input$noun1, input$verb, input$adjective, input$adverb)
+    generate_story(input$noun1, input$verb, input$adjective, input$adverb, input$number, input$country)
   })
   output$story <- renderText({
     story()
